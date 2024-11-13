f1c35bb7b4a32cd037c317698539ea3ce14d020e
V Lohar <loharvb@gmail.com>
2024-11-13 13:23:18 +0530

    Finding factorial number..

--- java_programs/Factorial_ Number/Factorial.java
+++ java_programs/Factorial_ Number/Factorial.java
@@ -8,9 +8,11 @@ public class Factorial {
     }
 
     public static int factorial(int n) {
+        // System.out.println(n);
         if (n == 0) {
             return 1;
         }
+        System.out.println(n * factorial(n - 1));
         return n * factorial(n - 1);
     }
 }
---
+++ java_programs/Fibonacci/Fibonacci.java
@@ -0,0 +1,12 @@
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fibonacciSum(n));
+    }
+
+    public static int fibonacciSum(int n) {
+
+    }
+}
