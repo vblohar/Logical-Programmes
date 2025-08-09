55da3d06ff1a111e4acf635a4dcb82d478abbca9
Vaibhav L <loharvb@gmail.com>
2025-08-09 23:34:27 +0530

    Finding fibonacci series..

--- java_programs/Fibonacci/Fibonacci.java
+++ java_programs/Fibonacci/Fibonacci.java
@@ -1,12 +1,35 @@
+import java.util.ArrayList;
 
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 20;
         System.out.println(fibonacciSum(n));
+
     }
 
-    public static int fibonacciSum(int n) {
-
+    public static ArrayList<Integer> fibonacciSum(int n) {
+        ArrayList<Integer> num = new ArrayList<>();
+        int first = 0;
+        int next = 1;
+        if (n == 0) {
+            num.add(first);
+            return num;
+        } else if ( n == 1){
+            num.add(first);
+            num.add(next);
+            return num;
+        } else if (n > 1) {
+            num.add(first);
+            num.add(next);
+            while(n > 1){
+                int sum = first + next;
+                num.add(sum);
+                first = next;
+                next = sum;
+                n--;
+            }
+        }
+        return num;
     }
 }
