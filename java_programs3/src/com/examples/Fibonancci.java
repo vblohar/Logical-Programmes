99da6b72d57e9d9f19bd601203afc887b7567129
Vaibhav L <loharvb@gmail.com>
2025-12-07 11:29:49 +0530

    How to get fibonacci series using recursion..

---
+++ java_programs3/src/com/examples/Fibonancci.java
@@ -0,0 +1,32 @@
+package com.examples;
+
+import java.util.List;
+
+public class Fibonancci {
+
+    public static int fibonacciSeries(int n) {
+        if(n <= 1){
+            return n;
+        }
+        return fibonacciSeries(n-1) + fibonacciSeries(n-1);
+    };
+
+    public static void main(String[] args) {
+//        int n1 = 0, n2 = 1, count = 10;
+//        System.out.print(n1);
+//        for (int i = 2; i < count; i++) {
+//            int n3 = n1 + n2;
+//            System.out.print(" " + n3);
+//            n1 = n2;
+//            n2 = n3;
+//        }
+//        int n =6;
+//           for (int i = 0; i < n; i++) {
+//            System.out.println(fibonacciSeries(i));
+//        }
+
+        System.out.println(fibonacciSeries(5));
+    }
+}
+
+
